cda14a3b4f9a5f3c51a978dac5b999060e9d4c3a
swasilyev <swasilyev@gmail.com>
2018-11-13 15:46:14 -0800

    script to convert signatures to libsnark format added

---
+++ src/eddsa/convert.py
@@ -0,0 +1,20 @@
+import argparse
+
+from generate import toBinaryString, verify
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Converts the signature to the libsnark format")
+    parser.add_argument("value", type=int)
+    parser.add_argument("R", help="point part of the signature", type=int, nargs=2)
+    parser.add_argument("S", help="scalar part of the signature", type=int)
+    parser.add_argument("pk", help="public key", type=int, nargs=2)
+    args = parser.parse_args()
+    print(args)
+    with open('keys/signature', 'w') as f:
+        print(toBinaryString(args.value), file = f)
+        print(toBinaryString(args.pk[0]), file = f)
+        print(toBinaryString(args.pk[1]), file = f)
+        print(toBinaryString(args.R[0]), file = f)
+        print(toBinaryString(args.R[1]), file = f)
+        print(toBinaryString(args.S)[::-1], file = f)
